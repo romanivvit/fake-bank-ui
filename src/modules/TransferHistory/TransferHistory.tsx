209a449a7f4025155028e85aee9891731563e603
Vitalii Romaniv <vitalii.romaniv@authenticgaming.com>
2023-05-22 09:50:58 +0300

    Changed naming

--- src/modules/TransferHistory/TransferHistory.tsx
+++ src/modules/TransferHistory/TransferHistory.tsx
@@ -2,22 +2,14 @@ import './styles.scss';
 import {useSelector} from "react-redux";
 import {useOperation} from "../../hooks/useOperation";
 import {selectOperationsData} from "../../store/slices/operation";
-import {useAppDispatch} from "../../store/hooks";
 export const TransferHistoryPage = () => {
-    const dispatch = useAppDispatch();
     const operationData = useSelector(selectOperationsData)
 
     useOperation();
 
-
-    // "created_at": "2023-05-21T13:32:46.082Z",
-    //     "operation_id": 0,
-    //     "receiver_account_id": 0,
-    //     "sender_account_id": 0,
-    //     "value": 0
     return (
         <div className='transfer-page'>
-            <h1>Transfer</h1>
+            <h1>Transfer History</h1>
             <div className="transfer-container">
                 <div className="card">
                     <div className="login-form">
